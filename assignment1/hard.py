55dc001f7f28e054420758dac2bb465fb699182d
hyojaeKwon <khj010909@gm.gist.ac.kr>
2021-09-30 02:31:27 +0900

    assignment 1

--- assignment1/hard.py
+++ assignment1/hard.py
@@ -1,7 +1,26 @@
-import sys
-Input = sys.stdin.readline
 
-pin = list(map(int,Input().split()))
+nums = [3,1,5,8]
 
-answer = 0
-for i in range(len(pin)):
+def matrixSolve(nums):
+    newNums = []
+    newNums.append(1)
+    for i in nums:
+        newNums.append(i)
+    newNums.append(1)
+    length = len(newNums)
+    matrix = [[0] * length for _ in range(length)]
+
+
+    for i in range(1,length):
+        for j in range(1,length - i):
+            k = i + j
+            matrix[j][k] = 0
+            for q in range(j,k):
+                cost = matrix[j][q] + matrix[q + 1][k] + newNums[j-1] * newNums[q] * newNums[k]
+                matrix[j][k] = max(cost, matrix[j][k])
+
+    print(matrix[1][length - 1])
+
+
+
+matrixSolve(nums)
