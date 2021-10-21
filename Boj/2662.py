30eb4d3d451f43f3e871e3fc96dc8f24045f0ed8
hyojaeKwon <khj010909@gm.gist.ac.kr>
2021-10-21 15:19:11 +0900

    knapsack

--- Boj/2662.py
+++ Boj/2662.py
@@ -3,20 +3,21 @@ Input = sys.stdin.readline
 
 n,m = map(int,Input().split())
 
-money = []
+
+#Knapsack dp changing
+money = [[0 for _ in range(m+1)]]
 for i in range(n):
     money.append(list(map(int,Input().split())))
 
-dp = [[0]*(sum(money[n-1])+1) for _ in range(n+1)]
-print(money)
-print(dp[0])
-for i in range(1,n+1):
-    for j in range(1,len(dp[0])):
-        for k in range(1,m+1):
-            if i > j:
-                dp[i][j]=dp[i-1][j]
-            else:
-                dp[i][j] = max(dp[i-1][j],dp[i-1][j-money[i][0]]+money[i][k])
+dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
 
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        dp[i][j] = max(dp[i][j-1],money[i][j])
+        for k in range(1,i+1):
+                       else:
+                sol[j]
+            if dp[i][j] < money[i-k][j] + dp[k][j-1]:
+                dp[i][j] = money[i-k][j] + dp[k][j-1]
 
 print(dp[-1][-1])
