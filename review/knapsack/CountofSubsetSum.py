88209e496ee6d8a86861ddef3c087d8a93697770
hyojaeKwon <khj010909@gm.gist.ac.kr>
2021-10-25 19:01:18 +0900

    [revise] knapsack advanced problem

--- review/knapsack/CountofSubsetSum.py
+++ review/knapsack/CountofSubsetSum.py
@@ -9,7 +9,7 @@ dp = [[0]*(sum(inList)+1) for _ in range(len(inList)+1)]
 dp[0][0] = 1
 
 for i in range(1,len(inList)+1):
-    for j in range(1,sum(inList)+1):
+    for j in range(sum(inList)+1):
         if inList[i-1] > j:
             dp[i][j] = dp[i-1][j]
         else:
@@ -17,7 +17,6 @@ for i in range(1,len(inList)+1):
                 dp[i][j] = 1
             else:
                 dp[i][j] = 0
-
 for i in range(sum(inList)+1):
     if dp[len(inList)][i]:
         print(i)
--- review/knapsack/minumumSubsetSumDiff.py
+++ review/knapsack/minumumSubsetSumDiff.py
@@ -7,5 +7,18 @@ inList = list(map(int,Input().split()))
 
 dp = [[0]*(sum(inList)//2+1) for _ in range(len(inList)+1)]
 
-for i in range(1,)
+dp[0][0] = 1
 
+for i in range(1,len(inList)+1):
+    for j in range(sum(inList)//2+1):
+        if inList[i-1] > j:
+            dp[i][j] = dp[i-1][j]
+        else:
+            if dp[i-1][j] or dp[i-1][j-inList[i-1]]:
+                dp[i][j] = 1
+            else:
+                dp[i][j] = 0
+
+for i in range(1,sum(inList)//2+1,-1):
+    if dp[-1][i]:
+        print(2*(sum(inList)//2 - i))
