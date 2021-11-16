9076058fa561208d98675186096cf9aa4dbfd711
hyojaeKwon <khj010909@gm.gist.ac.kr>
2021-11-16 14:34:12 +0900

    [update] bonus.py code update

--- greedy/bonus.py
+++ greedy/bonus.py
@@ -8,6 +8,13 @@ def bouns(eva):
     for i in range(len(eva)-1):
         if eva[i+1]>eva[i]:
             payTable[i+1]=payTable[i]+1
+
+        elif eva[i+1]==eva[i]:
+            if payTable[i+1]-1 < minValue:
+                payTable[i + 1] = payTable[i]
+                minValue = payTable[i+1]
+            else:
+                payTable[i + 1] = payTable[i] - 1
         else:
             payTable[i+1]=payTable[i]-1
             if minValue > payTable[i+1]:
